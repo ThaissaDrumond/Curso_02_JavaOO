d4138d48ffb8e7a87d3def68a1acdf85a38a924a
ThaissaDrumond <thaissa.villaverde@gmail.com>
2022-10-18 22:43:04 -0300

    Aula 03: Métodos

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -1,11 +1,32 @@
-import java.awt.font.TextHitInfo;
-
 public class Conta {
 
         double saldo;
-        int agencia = 42;
+        int agencia;
         int numero;
         String titular;
 
+        void deposita(double valor) {
+                this.saldo = this.saldo + valor;
+
+        }
+
+        public boolean saca(double valor) {
+                if (this.saldo >= valor) {
+                        this.saldo -= valor;
+                        return true;
+                } else {
+                        return false;
+                }
+        }
+
+        public boolean transfere(double valor, Conta destino) {
+                if (this.saldo >= valor) {
+                        this.saldo -= valor;
+                        destino.deposita(valor);
+                        return true;
+                } else {
+                        return false;
+                }
+        }
 
 }
